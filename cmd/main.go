e937e603b0f09b9547b386888f4f750a8c30eeb0
Andrei Sousa <sousandrei@gmail.com>
2022-10-03 20:03:02 +0200

    chore: add config and separate command into file (#5)

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,30 @@ package main
 import (
 	"fmt"
 
+	"github.com/kelseyhightower/envconfig"
 	"github.com/sousandrei/paulobaierbot/internal/bot"
 )
 
+type Config struct {
+	TelegramToken string
+}
+
 func main() {
-	bot, err := bot.New()
+	var cfg Config
+	err := envconfig.Process("myapp", &cfg)
+	if err != nil {
+		fmt.Println("error processing envconfig: %w", err)
+		return
+	}
+
+	bot, err := bot.New(cfg.TelegramToken)
 	if err != nil {
 		fmt.Println("Error creating bot", err)
+		return
 	}
 
 	if err := bot.Run(); err != nil {
 		fmt.Println("Error running bot:", err)
+		return
 	}
 }
